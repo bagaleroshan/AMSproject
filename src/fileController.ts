a9d0c404333e3c14e9cd7acd3a0ed84252c97fa2
kctsohil <kctsohil@gmail.com>
2024-05-06 11:47:49 +0545

    fileupdate

--- src/fileController.ts
+++ src/fileController.ts
@@ -1,13 +1,19 @@
-
-
-
-export let singleFileController = async (req:any, res:any, next:any) => {
-  let link = `http://localhost:8000/${req.file.filename}`;
-  res.json({ success: true, result: link });
+export let singleFileController = async (req: any, res: any, next: any) => {
+  try {
+    let link = `http://localhost:8000/${req.file.filename}`;
+    res.json({ success: true, result: link });
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+  }
 };
-export let multipleFileController = async (req:any, res:any, next:any) => {
-console.log(req.files)
-let link=req.files.map((val:any,i:any)=>{ return `http://localhost:8000/${val.filename}`;})
- res.json({result:link})
+export let multipleFileController = async (req: any, res: any, next: any) => {
+  console.log(req.files);
+  try {
+    let link = req.files.map((val: any, i: any) => {
+      return `http://localhost:8000/${val.filename}`;
+    });
+    res.json({ result: link });
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+  }
 };
-
