5c60f515a0eca76d1024bafdc9717b38cba4738d
kctsohil <kctsohil@gmail.com>
2024-05-13 11:10:27 +0545

    file delete success

--- src/Routes/fileRouter.ts
+++ src/Routes/fileRouter.ts
@@ -4,18 +4,19 @@ import {
   singleFileController,
 } from "../Controllers/fileController";
 import chkFol from "../utils/chkfun";
+import { fileHandleDelete } from "../utils/deleteFile1";
 import upload from "../utils/uploadFile";
-import { handleFileUpload } from "../utils/deleteFile";
 
 let file = Router();
 file
   .route("/single")
-  .post(chkFol, upload.single("document"), singleFileController);
+  .post(chkFol, upload.single("document"), singleFileController)
+
 
 file
   .route("/multiple")
   .post(chkFol, upload.array("document"), multipleFileController);
 
-file.route("/delete").delete(handleFileUpload);
+file.route("/:fileName").delete(fileHandleDelete);
 
 export default file;
