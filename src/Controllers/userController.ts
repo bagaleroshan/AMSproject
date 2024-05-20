9f3cc96f14c202b96b841cc05efade10d5d65f23
anish98166 <anish98166@gmail.com>
2024-05-20 14:09:04 +0545

    User login management completed p1

--- src/Controllers/userController.ts
+++ src/Controllers/userController.ts
@@ -19,6 +19,8 @@ import { myMongooseQuerys } from "../utils/mongooseQuery";
 import { attachments, sendEmail } from "../utils/sendMail";
 import { User } from "../Schema/model";
 import jwt from "jsonwebtoken";
+import { AuthenticatedRequest } from "../middleware/isAuthenticated";
+import { userInfo } from "os";
 
 export const createUserController = asyncHandler(
   async (req: Request, res: Response) => {
@@ -48,8 +50,7 @@ export const createUserController = asyncHandler(
       res,
       "Successfully created User and Verification email has been sent.",
       201,
-      result,
-      ""
+      result
     );
   }
 );
@@ -83,13 +84,102 @@ export let loginUserController = asyncHandler(
 );
 
 export const myProfile = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
-    let tokenString = req.headers.authorization || "";
-    let token = tokenString.split(" ")[1];
-    let user: any = await jwt.verify(token, secretKey);
-    let userId = user._id;
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    let userId = req._id;
     let result = await User.findById(userId);
-    successResponseData(res, "Profile read successfully", 200, result, "");
+    successResponseData(res, "Profile read successfully", 200, result);
+  }
+);
+
+export const updateProfile = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    let userId = req._id;
+    let data = req.body;
+    delete data.email;
+    delete data.password;
+    delete data.role;
+
+    let result = await User.findByIdAndUpdate(userId, data, {
+      new: true,
+    });
+    successResponseData(res, "Profile updated successfully", 201, result);
+  }
+);
+
+export const updatePassword = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    let userId = req._id;
+    let oldPassword = req.body.oldPassword;
+    let newPassword = req.body.newPassword;
+    let data = await User.findById(userId);
+    let dbPassword = data.password;
+    let isValidPassword = await bcrypt.compare(oldPassword, dbPassword);
+    if (isValidPassword) {
+      let newHashPassword = await bcrypt.hash(newPassword, 10);
+
+      let result = await User.findByIdAndUpdate(
+        userId,
+        {
+          password: newHashPassword,
+        },
+        {
+          new: true,
+        }
+      );
+      successResponseData(res, "Password updated successfully", 200, result);
+    }
+  }
+);
+
+export const forgotPassword = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    let email = req.body.email;
+    let result = await User.findOne({ email: email });
+    if (result !== null) {
+      let infoObj = {
+        _id: result._id,
+      };
+      let expiryInfo = {
+        expiresIn: "1d",
+      };
+      let token = await jwt.sign(infoObj, secretKey, expiryInfo);
+
+      await sendEmail({
+        from: "jenishona",
+        to: email,
+        subject: "Forgot Password",
+        html: `
+        <h4>Please verify that its you trying to reset your password.</h4><br/>
+        <a href="${clientUrl}/admin/reset-password?token=${token}">
+        ${clientUrl}/reset-password?token=${token}
+        </a><br/>
+        <h4>If you did not do this don't do anything.</h4><br/>
+        `,
+        attachments,
+      });
+      successResponseData(
+        res,
+        "Password reset link has been sent to your email.",
+        201,
+        result
+      );
+    }
+  }
+);
+
+export const resetPassword = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    let hashPassword = await bcrypt.hash(req.body.password, 10);
+    let result = await User.findByIdAndUpdate(
+      req._id,
+      {
+        password: hashPassword,
+      },
+      {
+        new: true,
+      }
+    );
+    successResponseData(res, "Password reset successfully", 201, result);
   }
 );
 
@@ -97,26 +187,26 @@ export const readAllUserController = asyncHandler(
   async (req: Request, res: Response) => {
     const { page, limit, sort, select, find } = myMongooseQuerys(req.query);
     let result = await readAllUserService(page, limit, sort, select, find);
-    successResponseData(res, "Successfully Read All User", 200, result, "");
+    successResponseData(res, "Successfully Read All User", 200, result);
   }
 );
 
 export const readSpecificUserController = asyncHandler(
   async (req: Request, res: Response) => {
     let result = await readSpecificUserService(req.params.id);
-    successResponseData(res, "Read Successfully", 200, result, "");
+    successResponseData(res, "Read Successfully", 200, result);
   }
 );
 
 export const updateUserController = asyncHandler(
   async (req: Request, res: Response) => {
     let result = await updateUserService(req.params.id, req.body);
-    successResponseData(res, "Successfully Updated", 201, result, "");
+    successResponseData(res, "Successfully Updated", 201, result);
   }
 );
 export const deleteUserController = asyncHandler(
   async (req: Request, res: Response) => {
     let result = await deleteUserService(req.params.id);
-    successResponseData(res, "Successfully Deleted", 200, result, "");
+    successResponseData(res, "Successfully Deleted", 200, result);
   }
 );
---
+++ src/middleware/isAuthenticated.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { secretKey } from "../utils/constant";
+import { NextFunction, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+
+export interface AuthenticatedRequest extends Request {
+  _id?: string;
+}
+
+const isAuthenticated = asyncHandler(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    let tokenString = req.headers.authorization || "";
+    if (!tokenString) {
+      throw new Error("No token provided");
+    }
+    let token = tokenString.split(" ")[1];
+    let user: any = await jwt.verify(token, secretKey);
+    req._id = user._id;
+
+    next();
+  }
+);
+
+export default isAuthenticated;
