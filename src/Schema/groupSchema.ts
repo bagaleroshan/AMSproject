e356a6740663f7c1728ea13a8dc74a7db007c61d
anish98166 <anish98166@gmail.com>
2024-06-17 14:29:26 +0545

    group start and end time validation added

--- src/Schema/groupSchema.ts
+++ src/Schema/groupSchema.ts
@@ -1,16 +1,30 @@
 import { Schema } from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
+interface IGroup extends Document {
+  subject: Schema.Types.ObjectId;
+  teacher: Schema.Types.ObjectId;
+  groupName: string;
+  students: string[];
+  active: boolean;
+  startTime: string;
+  endTime: string;
+}
+
+const startTimeValidator = function (this: IGroup, startTime: string): boolean {
+  return startTime < this.endTime;
+};
+
 const groupSchema: Schema = new Schema(
   {
     subject: {
       required: [true, "subject is Required"],
-      type: Schema.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "Subject",
     },
     teacher: {
       required: [true, "teacher is Required"],
-      type: Schema.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: "User",
     },
     groupName: {
@@ -30,6 +44,12 @@ const groupSchema: Schema = new Schema(
     startTime: {
       type: String,
       required: true,
+      validate: [
+        {
+          validator: startTimeValidator,
+          message: "Start time must be before end time",
+        },
+      ],
     },
     endTime: {
       type: String,
